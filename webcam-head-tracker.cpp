7ed126b19bbd3264373d675435f42411ae72ff96
Martin Lambers <marlam@marlam.de>
2026-08-01 01:29:25 +0200

    Fix compatibility with latest OpenCV and dlib

--- webcam-head-tracker.cpp
+++ webcam-head-tracker.cpp
@@ -30,7 +30,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/video/tracking.hpp>
 
 #include <dlib/opencv.h>
